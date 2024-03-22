c919e31cac2f58d75a904198b797a35fb4abb160
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-03-22 18:18:52 +0900

    feat: bump @fingerprintjs/fingerprintjs-pro-spa to 1.3.1

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,14 @@
-import type { FpjsClientOptions } from '@fingerprintjs/fingerprintjs-pro-spa'
+import type { FpjsSpaOptions } from '@fingerprintjs/fingerprintjs-pro-spa'
 
-export const getOptions = (options: FpjsClientOptions, packageName: string, version: string) => {
-  const clientOptions: FpjsClientOptions = {
+export const getOptions = (options: FpjsSpaOptions, packageName: string, version: string) => {
+  const loadOptions = {
+    ...(options?.loadOptions ?? {}),
+    integrationInfo: [...(options.loadOptions?.integrationInfo ?? []), `${packageName}/${version}`],
+  } as FpjsSpaOptions['loadOptions']
+
+  const clientOptions: FpjsSpaOptions = {
     ...options,
-    loadOptions: {
-      ...options.loadOptions,
-      integrationInfo: [...(options.loadOptions?.integrationInfo ?? []), `${packageName}/${version}`],
-    },
+    loadOptions,
   }
   return clientOptions
 }
