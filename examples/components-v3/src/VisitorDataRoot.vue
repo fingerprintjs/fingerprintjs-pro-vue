20756534643394c374d3512069af593a85c1070e
Juraj Uhlar <juro.uhlar@gmail.com>
2026-04-09 17:43:21 +0100

    chore: modernize examples and update dependencies
    Simplify Vue components, update example dependency versions,
    and fix developer documentation commands.

--- examples/components-v3/src/VisitorDataRoot.vue
+++ examples/components-v3/src/VisitorDataRoot.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import VisitorInfoCompositionApi from './VisitorInfoCompositionApi.vue'
 import VisitorInfoOptionsApi from './VisitorInfoOptionsApi.vue'
 </script>
 
 <template>
   <div class="visitor-data-root">
-    <visitor-info-options-api />
-    <visitor-info-composition-api />
+    <VisitorInfoOptionsApi />
+    <VisitorInfoCompositionApi />
   </div>
 </template>
 
--- examples/nuxt-v3-example/plugins/fingerprint.client.ts
+++ examples/nuxt-v3-example/plugins/fingerprint.client.ts
@@ -1,15 +1,13 @@
-import { fpjsPlugin, FpjsVueOptions } from '@fingerprintjs/fingerprintjs-pro-vue-v3'
-import { defineNuxtPlugin, Plugin, useRuntimeConfig } from 'nuxt/app'
+import { fpjsPlugin } from '@fingerprintjs/fingerprintjs-pro-vue-v3'
+import type { FpjsVueOptions } from '@fingerprintjs/fingerprintjs-pro-vue-v3'
+import { defineNuxtPlugin, useRuntimeConfig } from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
-  nuxtApp.vueApp.use(
-    fpjsPlugin as unknown as Plugin,
-    {
-      loadOptions: {
-        apiKey: config.public.API_KEY,
-      },
-    } as FpjsVueOptions
-  )
+  nuxtApp.vueApp.use(fpjsPlugin, {
+    loadOptions: {
+      apiKey: config.public.API_KEY,
+    },
+  } as FpjsVueOptions)
 })
